24ae5594061f8719bc9920771991c6362b5288cf
Neto test <uzdemir.a@yandex.ru>
2024-02-12 11:30:02 +0400

    first commit

---
+++ src/main/java/ru/netology/MovieManager.java
@@ -0,0 +1,44 @@
+package ru.netology;
+import java.util.Arrays;
+
+public class MovieManager {
+    private Movie[] movies;
+    private int limit;
+
+    public MovieManager() {
+        this.limit = 5;
+        this.movies = new Movie[0];
+    }
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+        this.movies = new Movie[0];
+    }
+
+    public void addMovie(Movie movie) {
+        Movie[] newMovies = new Movie[movies.length + 1];
+        System.arraycopy(movies, 0, newMovies, 0, movies.length);
+        newMovies[movies.length] = movie;
+        movies = newMovies;
+    }
+
+    public Movie[] findAll() {
+        return movies;
+    }
+
+    public Movie[] findLast() {
+        int resultSize = Math.min(limit, movies.length);
+        Movie[] result = new Movie[resultSize];
+
+        for (int i = 0; i < resultSize; i++) {
+            result[i] = movies[movies.length - 1 - i];
+        }
+
+        return result;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+}
+
---
+++ src/test/java/ru/netology/MovieManagerTest.java
@@ -0,0 +1,72 @@
+package ru.netology;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MovieManagerTest {
+    private MovieManager movieManager;
+
+    @Before
+    public void setUp() {
+        movieManager = new MovieManager();
+    }
+
+    @Test
+    public void testAddMovie() {
+        // Создаем фильмы для теста
+        Movie movie1 = new Movie("Фильм 1");
+        Movie movie2 = new Movie("Фильм 2");
+        Movie movie3 = new Movie("Фильм 3");
+
+        // Добавляем фильмы в менеджер афиши
+        movieManager.addMovie(movie1);
+        movieManager.addMovie(movie2);
+        movieManager.addMovie(movie3);
+
+        // Проверяем, что фильмы успешно добавлены
+        Assert.assertArrayEquals(new Movie[]{movie1, movie2, movie3}, movieManager.findAll());
+    }
+
+    @Test
+    public void testFindAll() {
+        Movie movie1 = new Movie("Фильм 1");
+        Movie movie2 = new Movie("Фильм 2");
+        Movie movie3 = new Movie("Фильм 3");
+
+        movieManager.addMovie(movie1);
+        movieManager.addMovie(movie2);
+        movieManager.addMovie(movie3);
+
+        Movie[] allMovies = movieManager.findAll();
+
+        Assert.assertArrayEquals(new Movie[]{movie1, movie2, movie3}, allMovies);
+    }
+
+    @Test
+    public void testFindLast() {
+        Movie movie1 = new Movie("Фильм 1");
+        Movie movie2 = new Movie("Фильм 2");
+        Movie movie3 = new Movie("Фильм 3");
+
+        movieManager.addMovie(movie1);
+        movieManager.addMovie(movie2);
+        movieManager.addMovie(movie3);
+
+        Movie[] lastMovies = movieManager.findLast();
+
+        Assert.assertArrayEquals(new Movie[]{movie3, movie2, movie1}, lastMovies);
+    }
+
+        @Test
+        public void testConstructorWithLimit() {
+            int limit = 3;
+
+            MovieManager movieManager = new MovieManager(limit);
+
+            // Проверка на установление ограничения
+            Assert.assertEquals(limit, movieManager.getLimit());
+
+            // Проверка, что при создании менеджера афиши не содержит фильмов
+            Assert.assertArrayEquals(new Movie[0], movieManager.findAll());
+        }
+    }
